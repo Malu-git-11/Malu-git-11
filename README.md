a36aa65250e9dad2e28998280e2cf47da947b0dd
Malu-git-11 <malumendesrodrigues13@gmail.com>
2025-11-29 21:08:02 -0300

    Projeto de TCC, refeito para poder ser publicado no github pages.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gpp-guaianases
-Projeto do TCC simplificado, para torna-lo online.
+PWA Firebase Auth (JavaScript, POO)
+
+O que tem aqui:
+- PWA (manifest + service worker)
+- Telas: index, register, login, profile
+- Autenticação Firebase (Email/Password) usando classe AuthService (POO)
+- Arquivos JS usam módulos (type="module")
---
+++ authService.js
@@ -0,0 +1,27 @@
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+class FirebaseAuthService {
+    #auth;
+
+    constructor(app) {
+        this.#auth = getAuth(app);
+    }
+
+    async criarUsuario(email, senha) {
+        return createUserWithEmailAndPassword(this.#auth, email, senha);
+    }
+
+    async login(email, senha) {
+        return signInWithEmailAndPassword(this.#auth, email, senha);
+    }
+
+    async logout() {
+        return signOut(this.#auth);
+    }
+}
+
+export default FirebaseAuthService;
