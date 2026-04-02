7b89aaff4fbe9075d28ba99fd3b0c6dde0b7600a
Maria Luisa <Maluworkq@gmail.com>
2026-04-02 20:36:47 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,11 +3,9 @@ const BASE_PATH = '/Malu-git-11'; // <- caminho do repositório
 
 const FILES_TO_CACHE = [
   `${BASE_PATH}/index.html`,
-  `${BASE_PATH}/perfil.html`,
   `${BASE_PATH}/style.css`,
   `${BASE_PATH}/perfil.css`,
   `${BASE_PATH}/app.js`,
-  `${BASE_PATH}/authService.js`,
   `${BASE_PATH}/manifest.json`
 ];
 
